be3480deb2c44539b84d57cb3a1d079f1ce16ba8
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-09 15:51:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 df = conn.read(worksheet="2147061722")
 
 if st.button("New Worksheet"):
-    conn.create(worksheet="Orders", data=orders)
+    conn.create(worksheet="1874208743", data=orders)
     st.success("Worksheet Created 🎉")
 
 
