896a95c3f6f0131726590aa3049a40609d5974c2
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-09 15:19:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(worksheet="Gatherizer",ttl="10m",usecols=[0, 1],nrows=1)
+df = conn.read(worksheet="Gatherizer")
 
 # Print results.
 for row in df.itertuples():
