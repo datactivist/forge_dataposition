75289c3f5fcbb83db8244ed4046bf91d4ee91529
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-10 19:07:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,3 @@ if st.button("New Worksheet"):
     st.success("Worksheet Created 🎉")
 
 st.dataframe(df)
-
-
-
-
-add = Block(name='Addition')
-sub = Block(name='Subtraction')
-mul = Block(name='Multiplication')
-div = Block(name='Division')
-
-barfi_result = st_barfi(base_blocks= [add, sub, mul, div])
-# or if you want to use a category to organise them in the frontend sub-menu
-barfi_result = st_barfi(base_blocks= {'Op 1': [add, sub], 'Op 2': [mul, div]})
