bd9242654f8fd709c25c050250c9b082fee441cb
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-11 19:11:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def colorizer_tab():
 
 def add_to_google_sheets(questions, answers, scores, values):
     data_to_add = [questions, answers, scores, values]
-    conn.create(data=data_to_add, worksheet_name="Colorizer")
+    conn.create(worksheet="Colorizer",data=data_to_add)
 
 def gatherizer_tab():
     st.title("Gatherizer Tab")
