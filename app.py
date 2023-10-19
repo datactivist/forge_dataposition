43cee0b8d58e4c2fdc429c5f83ff823db5a807d4
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-19 13:13:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def add_to_google_sheets(data):
     st.dataframe(existing_data)
 
     # Append the new data to the existing data
-    combined_df = pd.concat([existing_date, data], ignore_index=True)
+    combined_df = pd.concat([existing_data, data], ignore_index=True)
 
 
     # Update the Google Sheet with the combined data
