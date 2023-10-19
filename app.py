2e6cdf7726cb816c6e3352a2f844f455dac21cd3
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-19 12:35:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def colorizer_tab():
 
 @st.cache(suppress_st_warning=True)
 def add_to_google_sheets(data):
-    df = pd.Dataframe(data)
+    df = pd.DataFrame(data)
     conn.update(worksheet="Colorizer", data = df)
     st.cache_data_clear()
     
