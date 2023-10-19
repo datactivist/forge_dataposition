f2c588128e82c2435f10dddae4e051677f9b15ba
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-19 13:45:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ def colorizer_tab():
         combined_df = pd.concat([existing_df, new_df], ignore_index=True)
         st.dataframe(combined_df)
         conn.update(worksheet="Colorizer", data=combined_df)
-        st.session_state.data = {'question': [], 'answer': [], 'score': []}
         st.success("Data added to Google Sheets")
 
         
