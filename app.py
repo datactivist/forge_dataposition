31ef701b5412a980069d050f63e641c5c5846d9c
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-24 15:46:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def colorizer_tab():
                         st.session_state.profile['text_inputs'].append(text_inputs)
                         
                 st.write("List of Text Inputs:")
-                st.write(st.session_state.data['text_inputs'])
+                st.write(st.session_state.profile['text_inputs'])
             
         
 
