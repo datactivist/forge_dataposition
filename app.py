55f2eabb83f2e85541abc7b8652f5b03d4e41a07
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-24 17:49:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from streamlit_gsheets import GSheetsConnection
 import hydralit_components as hc
 import datetime
 from streamlit_image_coordinates import streamlit_image_coordinates
+from streamlit_elements import nivo, elements, mui, html
 
 # Make it look nice from the start
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
@@ -137,7 +138,62 @@ def gatherizer_tab():
 
 def dispenser_tab():
     st.title("Dispenser Tab")
+
+    DATA = [
+        { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
+        { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
+        { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
+        { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
+        { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
+    ]
+
+    with mui.Box(sx={"height": 500}):
+        nivo.Radar(
+            data=DATA,
+            keys=[ "chardonay", "carmenere", "syrah" ],
+            indexBy="taste",
+            valueFormat=">-.2f",
+            margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
+            borderColor={ "from": "color" },
+            gridLabelOffset=36,
+            dotSize=10,
+            dotColor={ "theme": "background" },
+            dotBorderWidth=2,
+            motionConfig="wobbly",
+            legends=[
+                {
+                    "anchor": "top-left",
+                    "direction": "column",
+                    "translateX": -50,
+                    "translateY": -40,
+                    "itemWidth": 80,
+                    "itemHeight": 20,
+                    "itemTextColor": "#999",
+                    "symbolSize": 12,
+                    "symbolShape": "circle",
+                    "effects": [
+                        {
+                            "on": "hover",
+                            "style": {
+                                "itemTextColor": "#000"
+                            }
+                        }
+                    ]
+                }
+            ],
+            theme={
+                "background": "#FFFFFF",
+                "textColor": "#31333F",
+                "tooltip": {
+                    "container": {
+                        "background": "#FFFFFF",
+                        "color": "#31333F",
+                    }
+                }
+            }
+        )
     # Add content for the Dispenser tab here
+
 
 # Create a function to display the selected tab content
 def display_tab_content(tab_label):
