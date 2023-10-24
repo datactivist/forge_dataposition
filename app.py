59ef859b712f9f853ff1fe489de108c47e9a7dab
ArthurSrZ <55806298+ArthurSrz@users.noreply.github.com>
2023-10-24 16:16:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,8 @@ def colorizer_tab():
                     st.session_state.profile['text_inputs'].append(text_input)
                         
             
+            for text_display in st.session_state.profile['text_inputs']:
+                st.metric(label="profile", value=text_display, delta=0, delta_color=colors.pop(0))
             #for every value inside st.session_state.profile['text_inputs'], print st.metric inside each column equally
             cola, colb, colc = st.columns(3)
             for text_display in st.session_state.profile['text_inputs']:
